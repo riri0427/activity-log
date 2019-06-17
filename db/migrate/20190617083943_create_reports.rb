00a275739f474c3b44d8cb94d0dab2363b1f092b
riri0427 <riri190427@gmail.com>
2019-06-17 17:56:03 +0900

    Update migration file of report model
    Remove unneccesary column

--- db/migrate/20190617083943_create_reports.rb
+++ db/migrate/20190617083943_create_reports.rb
@@ -7,7 +7,6 @@ class CreateReports < ActiveRecord::Migration[5.2]
       t.string      :image
       t.text        :comment
       t.references  :user,         foreign_key: true
-      t.references  :activity,     foreign_key: true
       t.timestamps
     end
   end
