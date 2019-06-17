ae08eb0cd6619be434ce6e43925d825573869d99
riri0427 <riri190427@gmail.com>
2019-06-17 17:52:39 +0900

    Update migration file of report model

--- db/migrate/20190617083943_create_reports.rb
+++ db/migrate/20190617083943_create_reports.rb
@@ -1,7 +1,13 @@
 class CreateReports < ActiveRecord::Migration[5.2]
   def change
     create_table :reports do |t|
-
+      t.string      :title,        index: true, null: false
+      t.integer     :spend_hour
+      t.integer     :spend_minute, null: false
+      t.string      :image
+      t.text        :comment
+      t.references  :user,         foreign_key: true
+      t.references  :activity,     foreign_key: true
       t.timestamps
     end
   end
