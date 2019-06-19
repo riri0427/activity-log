f82b6407cfdbc1c12e6180c36ecf6a059a1dcb93
riri0427 <riri190427@gmail.com>
2019-06-19 12:01:26 +0900

    Update reports_controller.rb
    Fix variable name

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,6 +1,6 @@
 class ReportsController < ApplicationController
   def index
-    @report = Report.includes(:user).order("created_at DESC")
+    @reports = Report.includes(:user).order("created_at DESC")
   end
 
   def new
