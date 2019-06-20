177a9534eab055c23e8eb548dd931ccda7db79e3
riri0427 <riri190427@gmail.com>
2019-06-20 17:56:24 +0900

    Update routes.rb
    Set routing of show action of users controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'reports#index'
   resources :reports, only: [:index, :show, :new, :create, :edit, :update]
+  resources :users, only: [:show]
 end
