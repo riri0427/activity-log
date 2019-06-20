3e7b20ed3142968b1c90cc5f9e966e94c389d5fb
riri0427 <riri190427@gmail.com>
2019-06-20 12:14:05 +0900

    Update migration file of report model

--- db/migrate/20190620024515_change_column_to_report.rb
+++ db/migrate/20190620024515_change_column_to_report.rb
@@ -1,6 +1,6 @@
 class ChangeColumnToReport < ActiveRecord::Migration[5.2]
   def up
-    change_column :reports, :spend_hour, :integer, default: 0
+    change_column :reports, :spend_hour, :integer, default: "0"
   end
 
   def down
