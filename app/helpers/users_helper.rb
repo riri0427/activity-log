bbb2fea013de3149a251fbee818c905af29f16cd
riri0427 <riri190427@gmail.com>
2019-06-21 11:56:00 +0900

    Update users_helper.rb
    Remove unneccesary written

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,8 +3,8 @@ module UsersHelper
     spend_hour = user_reports.sum(:spend_hour) * 60
     spend_minute = user_reports.sum(:spend_minute)
     spend_time_sum = spend_hour + spend_minute
-    @result_spend_hour = (spend_time_sum / 60).floor
-    @result_spend_minute = spend_time_sum % 60
+  end
+
   def return_result_spend_hour(user_reports)
     spend_time_sum = calculate_spend_time(user_reports)
     result_spend_hour = (spend_time_sum / 60).floor
