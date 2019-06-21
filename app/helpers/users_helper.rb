e9dc34d05f3c49982f93d0729b34a279efdf06fb
riri0427 <riri190427@gmail.com>
2019-06-21 10:52:26 +0900

    Update users_helper.rb
    Define calculate_spend_time in detail

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,9 @@
 module UsersHelper
-  def calculate_spend_time
-    
+  def calculate_spend_time(user_reports)
+    spend_hour = user_reports.sum(:spend_hour) * 60
+    spend_minute = user_reports.sum(:spend_minute)
+    spend_time_sum = spend_hour + spend_minute
+    @result_spend_hour = (spend_time_sum / 60).floor
+    @result_spend_minute = spend_time_sum % 60
   end
 end
