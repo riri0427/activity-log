0cb446ac13004f1123d09f4100731884ca476839
riri0427 <riri190427@gmail.com>
2019-06-21 11:51:31 +0900

    Update users_helper.rb
    Define return_result_spend_hour

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,5 +5,8 @@ module UsersHelper
     spend_time_sum = spend_hour + spend_minute
     @result_spend_hour = (spend_time_sum / 60).floor
     @result_spend_minute = spend_time_sum % 60
+  def return_result_spend_hour(user_reports)
+    
+  end
   end
 end
