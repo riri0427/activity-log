e14648158719a7587fa2577ad880472a3f150870
riri0427 <riri190427@gmail.com>
2019-07-29 10:30:48 +0900

    Set routing of activities

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root 'reports#index'
   resources :reports, only: [:index, :show, :new, :create, :edit, :update]
-  resources :users, only: [:show]
+  resources :users, only: [:show] do
+    # 仮置き
+    member do
+      get 'activity'
+    end
+  end
+  resources :activities, only: [:new, :create]
 end
