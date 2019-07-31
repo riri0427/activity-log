37d3be48b455efc9b727761eb4297f3b6e460080
riri0427 <riri190427@gmail.com>
2019-07-31 19:28:19 +0900

    Modify local variable of show action in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
   def show
-    user = User.find(params[:id])
-    @nickname = user.nickname
-    @activities = user.activities
-    @reports = user.reports.order("created_at DESC")
+    @user = User.find(params[:id])
+    @nickname = @user.nickname
+    @activities = @user.activities
+    @reports = @user.reports.order("created_at DESC")
   end
 end
