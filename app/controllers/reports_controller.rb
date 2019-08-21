885c62a34afe8fc1c1ab70a9399693937b1561f4
riri0427 <riri190427@gmail.com>
2019-08-21 11:05:13 +0900

    Display pagination

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,6 +1,6 @@
 class ReportsController < ApplicationController
   def index
-    @reports = Report.includes(:user).order("created_at DESC")
+    @reports = Report.includes(:user).order("created_at DESC").page(params[:page]).per(10)
   end
 
   def show
